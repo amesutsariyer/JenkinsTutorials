1057b1a2ebfbb1acbe0ad0b43c7e1ceb1452750b
ahmet.sariyer <ahmet.sariyer@kafein.com.tr>
2018-12-26 15:10:42 +0300

    comment https

--- Jenkins.FirstApp/Startup.cs
+++ Jenkins.FirstApp/Startup.cs
@@ -40,7 +40,7 @@ namespace Jenkins.FirstApp
                 app.UseHsts();
             }
 
-            app.UseHttpsRedirection();
+            //app.UseHttpsRedirection();
             app.UseMvc();
         }
     }
